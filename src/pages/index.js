72f04721b5255d0f28048dd43b3b11c806cd64a4
Anup <anupkrjha@gmail.com>
2019-01-29 17:14:55 +0530

    Init

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,11 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Home" keywords={[`Anup Jha`, `application`, `react`]} />
+    <h6>Work in progress...</h6>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
