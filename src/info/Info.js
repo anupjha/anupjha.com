bc7d68c0a744ebe77589b8fae7f5799604b6821d
Anup Jha <anup@wrench.com>
2022-10-11 18:32:22 +0530

    :tada:

--- src/info/Info.js
+++ src/info/Info.js
@@ -27,7 +27,7 @@ export const info = {
   miniBio: [
     // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
     {
-      emoji: "☕",
+      emoji: "🍺",
       text: "fueled by Beer",
     },
     {
